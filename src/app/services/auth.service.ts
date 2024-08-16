279f1736912a0c1653e4fdef191c90ac92d7c64d
Camila Guzman <camilaguz54@gmail.com>
2024-08-16 09:26:02 -0300

    change route

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   
   constructor(private http: HttpClient) {}
 
-  private apiUrl = 'http://localhost:8081/api/v1/sessions';
+  private apiUrl = 'http://localhost:8081/api/v1/sessions/register';
 
   register(userData: any): Observable<any> {
     const headers = new HttpHeaders({
